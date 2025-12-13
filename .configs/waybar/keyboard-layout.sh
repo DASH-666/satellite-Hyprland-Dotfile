6ab9fc5380169bfaf1f14e3c7037924fd27c6342
DASH-666 <149830948+DASH-666@users.noreply.github.com>
2025-12-13 16:58:22 +0330

    Delete .configs/waybar/keyboard-layout.sh

--- .configs/waybar/keyboard-layout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-layout=$(hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap')
-
-case "$layout" in
-    "English (US)") echo "EN" ;;
-    "Persian") echo "FA" ;;
-    *) echo "$layout" ;;
-esac
